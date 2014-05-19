48faa4d862404fd2d0197562b3f4229fdf3a180c
nhenscheid <nhenscheid@math.arizona.edu>
2014-05-19 11:02:45 -0700

    added about and methodology

--- js/script.js
+++ js/script.js
@@ -25,6 +25,25 @@ $(function() {
     		});
     		
     		
+$( "#aboutlink" ).click(function() {
+  $( "#menu" ).fadeTo(500,0);
+  $( "#methodology" ).fadeTo(500,0);
+  $("#about").fadeTo(500,1);
+});
+
+$( "#homelink" ).click(function() {
+  $( "#menu" ).fadeTo(500,1);
+  $( "#methodology" ).fadeTo(500,0);
+  $("#about").fadeTo(500,0);
+});
+	
+$( "#methodologylink" ).click(function() {
+  $( "#menu" ).fadeTo(500,0);
+  $( "#methodology" ).fadeTo(500,1);
+  $("#about").fadeTo(500,0);
+});
+	
+	
 // EB: added this function
             function getSQL() {
                 // The idea is that we generate the SQL based on the checked boxes
